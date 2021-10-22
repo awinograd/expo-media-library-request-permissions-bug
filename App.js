f957c8a068a605389331c751a066bcacf7aee3f4
Alec Winograd <alec@ouitherapeutics.com>
2021-10-22 15:45:04 -0500

    reproduce MediaLibrary.requestPermissionsAsync bug

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Permissions from "expo-permissions";
+import * as MediaLibrary from "expo-media-library";
 
 export default function App() {
+  React.useEffect(() => {
+    async function main() {
+      const [r1, r2] = await Promise.all([
+        Permissions.askAsync(Permissions.MEDIA_LIBRARY_WRITE_ONLY),
+        MediaLibrary.requestPermissionsAsync(true),
+      ]);
+      console.log({ r1, r2 });
+    }
+    main();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
